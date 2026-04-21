82a0750d7bc25a1f8771cc425790b58e9bf3127e
Fazlurahmanck <fazlurahmanck4.com>
2026-04-21 17:00:16 +0530

    my first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import "./css/navbar.css";
+import "./css/front_hig.css";
+import "./css/course.css";
+import "./css/footer.css";
+import "./css/exe.css";
+import "./css/group.css";
+import "./css/we_are.css";
+import Mycomponent  from './App';  // import cheyyunnu
+import reportWebVitals from './reportWebVitals';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  //passcheyyunnu
+  <React.StrictMode>
+    <Mycomponent /> 
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
