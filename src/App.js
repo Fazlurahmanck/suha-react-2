91b89ba87b89510e5eb4c9804db4f96f73ad43eb
Fazlurahmanck <130503362+Fazlurahmanck@users.noreply.github.com>
2026-07-20 11:49:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ function MyComponent() {
 
         <div className="slide">
           <div className="slide-content">
-            <img src="images/adhil ck.jpg" alt="Suha Group Team Member" />
+            <img src="images/PLACEMENT NEW TEMPLATE ADHIL CK.jpg" alt="Suha Group Team Member" />
           </div>
         </div>
 
